7b346f0569be83ff820cd0e0665b62b0bebae83b
John-Mary Okocha <jmo.okocha@gmail.com>
2023-05-01 10:54:06 -0600

    first version

--- src/Assignments/ExternalFilesImport.java
+++ src/Assignments/ExternalFilesImport.java
@@ -1,41 +1,34 @@
 package Assignments;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.io.IOException;
+//import java.util.ArrayList;
 
 public class ExternalFilesImport {
-    //    https://www.kaggle.com/datasets/jacksonbarreto/higher-education-institutions-in-the-usa
-//    https://www.javatpoint.com/how-to-read-csv-file-in-java
 
-    private static ArrayList<String> fileImport = new ArrayList<>();
+//    private static ArrayList<String> schoolData = new ArrayList<>();
 
+    //https://www.kaggle.com/datasets/jacksonbarreto/higher-education-institutions-in-the-usa
+    //https://www.javatpoint.com/how-to-read-csv-file-in-java
+//    https://stackoverflow.com/questions/1505311/how-can-i-clean-up-misaligned-columns-in-text
     public static void main(String[] args) {
-        divider();
-        dataReader();
 
-        fileImport = dataReader("universityData.csv");
-        System.out.println(fileImport.get(1));
+        String line = "";
+        String splitBy = ",";
 
-    }
-
-    //Used for importing dataset
-    private static ArrayList<String> dataReader(String "universityData.csv") {
-        ArrayList<String> temp = new ArrayList<>();
         try {
-            BufferedReader br = new BufferedReader(new FileReader("universityData.csv"));
-            while(br.ready()) {
-                temp.add(br.readLine());
+            //BufferedReader
+            BufferedReader br = new BufferedReader(new FileReader("src\\universityData.csv"));
+            while((line = br.readLine()) != null) { // returns a Boolean Value
+            String[] schoolData = line.split(splitBy);
+            System.out.printf("\n School Name = " + schoolData[2] + "\t\t Region = " + schoolData[1] + "\t\t Type = " + schoolData[3] + "\t\t URL = " + schoolData[4]);
             }
-            br.close();
-        } catch (IOException e) {
-            System.out.println(e);
+        } catch(IOException e) {
+            e.printStackTrace();
         }
-        return temp;
-    }
 
-    private static void divider() {
-        System.out.println("---------------");
     }
 
 }
+
