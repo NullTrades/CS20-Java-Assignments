1d5156ffa1ec0e40b878f18a5e102516520ae472
John-Mary Okocha <jmo.okocha@gmail.com>
2023-05-02 10:27:48 -0600

    first version

--- src/Assignments/ExternalFilesImport.java
+++ src/Assignments/ExternalFilesImport.java
@@ -3,32 +3,47 @@ package Assignments;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-//import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExternalFilesImport {
 
-//    private static ArrayList<String> schoolData = new ArrayList<>();
+    private static ArrayList<Integer> schoolID = new ArrayList<>();
+    private static ArrayList<String> schoolRegion = new ArrayList<>();
+    private static ArrayList<String> schoolName = new ArrayList<>();
+    private static ArrayList<String> schoolCategory = new ArrayList<>();
 
     //https://www.kaggle.com/datasets/jacksonbarreto/higher-education-institutions-in-the-usa
     //https://www.javatpoint.com/how-to-read-csv-file-in-java
-//    https://stackoverflow.com/questions/1505311/how-can-i-clean-up-misaligned-columns-in-text
-    public static void main(String[] args) {
+    //https://stackoverflow.com/questions/1505311/how-can-i-clean-up-misaligned-columns-in-text
+    public static void main(String[] args)  {
 
         String line = "";
-        String splitBy = ",";
 
         try {
             //BufferedReader
             BufferedReader br = new BufferedReader(new FileReader("src\\universityData.csv"));
+
+            //Skips the first line (header information) in the .csv file
+            line = br.readLine();
+
             while((line = br.readLine()) != null) { // returns a Boolean Value
-            String[] schoolData = line.split(splitBy);
-            System.out.printf("\n School Name = " + schoolData[2] + "\t\t Region = " + schoolData[1] + "\t\t Type = " + schoolData[3] + "\t\t URL = " + schoolData[4]);
+                String[] schoolData = line.split(",");
+
+                schoolID.add(Integer.valueOf(schoolData[0])); // add first element to schoolId and other lists
+                schoolRegion.add(schoolData[1]); //Add second element to schoolName lists
+                schoolName.add(schoolData[2]);
+                schoolCategory.add(schoolData[3]);
             }
         } catch(IOException e) {
             e.printStackTrace();
         }
 
+        System.out.println(schoolID);
+        System.out.println(schoolRegion);
+        System.out.println(schoolName);
+        System.out.println(schoolCategory);
+
     }
 
 }
-
